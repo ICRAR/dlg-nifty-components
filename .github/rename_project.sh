92e96c7a6d5d26720f62e3912c8da20511d3804d
calgray <calgray@users.noreply.github.com>
2021-12-08 04:28:35 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ICRAR"
+original_name="dlg_nifty_components"
+original_urlname="dlg-nifty-components"
+original_description="Awesome dlg_nifty_components created by ICRAR"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv dlg_nifty_components $name
 
 # remove the template related documents
 rm ABOUT_THIS_TEMPLATE.md
--- dlg_nifty_components/__main__.py
+++ dlg_nifty_components/__main__.py
@@ -7,7 +7,7 @@ from . import MyAppDROP  # pragma: no cover
 def main() -> None:  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m dlg_nifty_components` and `$ dlg_nifty_components `.
 
     This is your program's entry point.
 
@@ -21,15 +21,15 @@ def main() -> None:  # pragma: no cover
         * Run an application (Flask, FastAPI, Django, etc.)
     """
     parser = argparse.ArgumentParser(
-        description="project_name.",
-        epilog="Enjoy the project_name functionality!",
+        description="dlg_nifty_components.",
+        epilog="Enjoy the dlg_nifty_components functionality!",
     )
     # This is required positional argument
     parser.add_argument(
         "name",
         type=str,
         help="The username",
-        default="author_name",
+        default="ICRAR",
     )
     # This is optional named argument
     parser.add_argument(
